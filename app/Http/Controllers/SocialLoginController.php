5042eff5bfb7c5a048045d292d095df05121ded5
Kamrul <khannahid361@gmail.com>
2024-11-13 15:02:50 +0600

    Laravel socialite package add done

---
+++ app/Http/Controllers/SocialLoginController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialLoginController extends Controller
+{
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback($provider)
+    {
+        $user = Socialite::driver($provider)->user();
+        $existingUser = User::where('email', $user->email)->first();
+        if ($existingUser) {
+            Auth::login($existingUser);
+        } else {
+
+            User::create([
+                'name'          => $user->name,
+                'email'         => $user->email,
+                'password'      => Hash::make(Str::random(8)),
+                'provider_name' => $provider,
+                'provider_id'   => $user->id,
+                'provider_token' => $user->token
+            ]);
+        }
+        return view('welcome');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,40 +6,41 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Crypt;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'provider_name',
+        'provider_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
+        'provider_token'
+
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function setProviderTokenAttribute($value){
+        return $this->attributes['provider_token'] = Crypt::crypt($value);
+    }
+
+    public function getProviderTokenAttribute($value)
+    {
+        return Crypt::decrypt($value);
+    }
 }
